eb843289fc64415fcb234051456dbac656763467
kong0107 <kong0107@gmail.com>
2013-07-06 01:36:41 +0800

    resolves bug due to my mis-set `run_at`
    *
    前一版不會預處理（例如判斷預設法規）全國法規資料庫（因為`LER.js`晚於`moj.js`執行），已修正`manifest.json`中的`run_at`。
    * 預計對全國法規資料庫也做排版（參閱`moj.js`末個註解）

--- moj.js
+++ moj.js
@@ -1,16 +1,33 @@
 ﻿(function(){
-    /** 在法規名稱處加上一個anchor
-      * 以找「第一個TH標籤」實作，後續或許可以有其他應用。
-      */
-    var firstTH = document.getElementsByTagName("TH")[0];
-    if(firstTH) 
-        firstTH.innerHTML = '<a name="firstTH">' + firstTH.innerText + '</a>';
-
     if(typeof LER != "object" || !LER.setDefaultLaw) return;
+    LER.setAutoParse(null);
 
     /// 判斷「預設法規」
     var pages = ["All", "Single", "AllPara", "SearchNo", "SearchContent", "History"];
     var match = document.location.pathname.match(/^\/LawClass\/Law(\w+).aspx/);
     if(!match || pages.indexOf(match[1]) < 0) return;
     LER.setDefaultLaw(document.getElementById("Content").getElementsByTagName('A')[1].lastChild.data.replace(/\s/g, ''));
+    
+    /** 在法規名稱處加上一個anchor
+      * 以找「第一個TH標籤」實作，後續或許可以有其他應用。
+      */
+    var firstTH = document.getElementsByTagName("TH")[0];
+    if(firstTH) 
+        firstTH.innerHTML = '<a name="firstTH">' + firstTH.innerText + '</a>';
+    
+    /** 處理把全國法規資料庫的斷行排版
+      * 嘗試排版承接層式表單（如`ly.js`）
+      * 處理中
+      */
+    /*var pres = document.getElementsByTagName("PRE");
+    while(pres.length) {
+        var list = document.createElement("OL");
+        list.className = "LER-art-paragraphs";
+        var lines = pres[0].innerText.split("\n");
+        for(var i = 0; i < lines.length; ++i) {
+            if(!lines[i].length) continue;
+            list.innerHTML += "<li>" + lines[i] + "</li>";
+        }
+        pres[0].parentNode.replaceChild(list, pres[0]);
+    }*/
 })();
